ca392c47b75bb716914de16dffda243f6623c26f
Jye019 <jihyekim019@gmail.com>
2020-06-24 21:41:59 +0900

    웹 구축 및 시작

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        if(err) {
+            throw err;
+        }
+        res.end(data);
+    });
+}).listen(3001, () => {
+    console.log('server : 3001포트에서 요청 대기 중')
+});
